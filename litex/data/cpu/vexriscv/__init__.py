60676efb8eb147741949e842f3372ac41856008d
LiteX <robot@timvideos.us>
2020-04-06 15:28:03 +0000

    Updated using 0.0.post43 from https://github.com/litex-hub/litex-data-auto

--- litex/data/cpu/vexriscv/__init__.py
+++ litex/data/cpu/vexriscv/__init__.py
@@ -2,12 +2,35 @@ import os.path
 __dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
 data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/SpinalHDL/VexRISCV.git"
-git_hash = "5f0c7a7faf6b65c907a93be6e3723e297d37ee71"
-git_describe = "v1.0.1-265-g5f0c7a7"
-version_str = "1.0.1.post265"
-version_tuple = (1, 0, 1)
+
+# Module version
+version_str = "1.0.1.post308"
+version_tuple = (1, 0, 1, 308)
 try:
     from packaging.version import Version as V
-    pversion = V("1.0.1.post265")
+    pversion = V("1.0.1.post308")
+except ImportError:
+    pass
+
+# Data version info
+data_version_str = "1.0.1.post265"
+data_version_tuple = (1, 0, 1)
+try:
+    from packaging.version import Version as V
+    pdata_version = V("1.0.1.post265")
+except ImportError:
+    pass
+data_git_hash = "5f0c7a7faf6b65c907a93be6e3723e297d37ee71"
+data_git_describe = "v1.0.1-265-g5f0c7a7"
+data_git_msg = """\
+
+"""
+
+# Tool version info
+tool_version_str = "0.0.post43"
+tool_version_tuple = (0, 0, 43)
+try:
+    from packaging.version import Version as V
+    ptool_version = V("0.0.post43")
 except ImportError:
     pass
--- test.py
+++ test.py
@@ -3,8 +3,18 @@ import os
 
 from litex.data.cpu import vexriscv
 
-print("Found vexriscv @ version", vexriscv.version_str, "("+vexriscv.git_hash+")")
+print("Found vexriscv @ version", vexriscv.version_str, "(with data", vexriscv.data_version_str, ")")
+print()
 print("Data is in", vexriscv.data_location)
 assert os.path.exists(vexriscv.data_location)
-print("It contains:\n -", end=" ")
-print("\n - ".join(os.listdir(vexriscv.data_location)))
+print("Data is version", vexriscv.data_version_str, vexriscv.data_git_hash)
+print("-"*75)
+print(vexriscv.data_git_msg)
+print("-"*75)
+print()
+print("It contains:")
+for root, dirs, files in os.walk(vexriscv.data_location):
+    dirs.sort()
+    for f in sorted(files):
+        path = os.path.relpath(os.path.join(root, f), vexriscv.data_location)
+        print(" -", path)
