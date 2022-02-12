cdfb2306087a680df65153760fd41002e316ad30
LiteX Robot <robot@timvideos.us>
2022-02-12 19:07:23 +0000

    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_cpu_vexriscv/__init__.py
+++ pythondata_cpu_vexriscv/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "verilog")
 src = "https://github.com/SpinalHDL/VexRISCV.git"
 
 # Module version
-version_str = "1.0.1.post389"
-version_tuple = (1, 0, 1, 389)
+version_str = "1.0.1.post391"
+version_tuple = (1, 0, 1, 391)
 try:
     from packaging.version import Version as V
-    pversion = V("1.0.1.post389")
+    pversion = V("1.0.1.post391")
 except ImportError:
     pass
 
@@ -27,11 +27,11 @@ data_git_msg = """\
 """
 
 # Tool version info
-tool_version_str = "0.0.post124"
-tool_version_tuple = (0, 0, 124)
+tool_version_str = "0.0.post126"
+tool_version_tuple = (0, 0, 126)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post124")
+    ptool_version = V("0.0.post126")
 except ImportError:
     pass
 
